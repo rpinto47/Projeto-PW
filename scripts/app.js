cff50f68cd71e949aa8d747b9f4d2c915917fbf4
rmaduro <202200166@estudantes.ips.pt>
2024-01-29 23:02:48 +0000

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -2,10 +2,22 @@
 const path = require("path");
 const express = require("express");
 const favicon = require("serve-favicon");
-const readTasks = require("./routes/read-tasks");
-const createTask = require("./routes/create-task");
-const updateTasks = require("./routes/update-task");
-const deleteTasks = require("./routes/delete-task");
+const deleteOrderItemsRoute = require("./routes/Order-items/delete-order-items");
+const getOrderItemsRoute = require("./routes/Order-items/get-order-items");
+const postOrderItemsRoute = require("./routes/Order-items/post-order-items");
+const putOrderItemsRoute = require("./routes/Order-items/put-order-items");
+const deleteProductTypesRoute = require("./routes/Products/delete-product-types");
+const deleteProductsRoute = require("./routes/Products/delete-products");
+const getProductTypesRoute = require("./routes/Products/get-product-types");
+const getProductsRoute = require("./routes/Products/get-products");
+const postProductTypesRoute = require("./routes/Products/post-product-types");
+const postProductsRoute = require("./routes/Products/post-products");
+const putProductTypesRoute = require("./routes/Products/put-product-types");
+const putProductsRoute = require("./routes/Products/put-products");
+const getTablesRoute = require("./routes/Tables/get-tables");
+const putTablesRoute = require("./routes/Tables/put-tables");
+
+
 
 const app = express();
 
@@ -18,6 +30,6 @@ app.post("/tasks", createTask);
 app.put("/tasks/:id", updateTasks);
 app.delete("/tasks/:id", deleteTasks);
 
-app.listen(8081, function () {
-    console.log("Server running at http://localhost:8081");
+app.listen(3000, function () {
+    console.log("Server running at http://localhost:3000");
 });
